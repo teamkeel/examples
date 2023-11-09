9b209550eede8f60b66156a7e656526a352a910b
Tejas Kumar <tejas@tejas.qa>
2023-11-09 10:26:46 +0100

    Update save document flow

--- apps/saas-starter/src/app/[teamId]/new/page.tsx
+++ apps/saas-starter/src/app/[teamId]/new/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { createDocument } from '@/app/actions/createDocument';
+import { NewDocumentButton } from '@/components/NewDocumentButton';
 import { Button } from '@/components/ui/button';
 import { redirect, useParams } from 'next/navigation';
 import { useFormState } from 'react-dom';
@@ -30,14 +31,14 @@ export default function NewDocumentPage() {
       <textarea
         required
         name="content"
-        className="h-full text-white bg-transparent"
+        className="h-full p-4 text-white bg-transparent"
         placeholder={`An essay about why I love Lorem Ipsum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
 
 Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.
 
 Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. `}
       ></textarea>
-      <Button>Save</Button>
+      <NewDocumentButton />
     </form>
   );
 }
