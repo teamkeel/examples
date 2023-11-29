0c197de511f6e93cb9d79d20c38bab202fb24adb
Tejas Kumar <tejas@tejas.qa>
2023-11-29 15:00:17 +0100

    Use a proper secret

--- apps/todomvc-remix/app/sessions.server.ts
+++ apps/todomvc-remix/app/sessions.server.ts
@@ -1,10 +1,14 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 
+if (!process.env.SIGNING_SECRET) {
+    throw new Error("SIGNING_SECRET is not set");
+}
+
 export const { getSession, commitSession, destroySession } =
     createCookieSessionStorage({
         cookie: {
             name: "__session",
-            secrets: ["r3m1xr0ck5"],
+            secrets: [process.env.SIGNING_SECRET],
             sameSite: "lax",
         },
     });
