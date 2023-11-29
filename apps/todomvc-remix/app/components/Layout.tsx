34bbc1bb4eb03429e1c900f9f9789bd9179b3991
Tejas Kumar <tejas@tejas.qa>
2023-11-29 14:54:07 +0100

    Tidy up

--- apps/todomvc-remix/app/components/Layout.tsx
+++ apps/todomvc-remix/app/components/Layout.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from "react";
+import { KeelLogo } from "./KeelLogo";
 
 export function Layout({ children }: PropsWithChildren<{}>) {
   return (
     <div className="container">
       <header style={{ paddingBlock: "2rem" }}>
-        <h1 style={{ margin: 0 }}>
-          Keel Logo x <span>TodoMVC</span>
+        <h1
+          style={{ margin: 0, display: "flex", alignItems: "center", gap: 16 }}
+        >
+          <KeelLogo /> x <span>TodoMVC</span>
         </h1>
       </header>
       <main>{children}</main>
